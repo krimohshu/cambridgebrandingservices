52a05792dcff4644e7923bc53df398210d6ab28c
Vikas Shrivastava <vikasrehli@gmail.com>
2023-01-08 21:04:47 +0000

    latest commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+Insert into cbs_user (id,name,password,email)
+VALUES
+('1','cbsapp','9999','cbs@gmail.com');
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,8 @@
+CREATE TABLE cbs_user
+  (  
+     id					int,
+     name               varchar(255),
+     password           varchar(255),
+     email              varchar(255),
+     PRIMARY KEY(id)
+  );
