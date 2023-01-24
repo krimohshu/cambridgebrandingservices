21dccb0bd8d67079d4e177b1d9ab0bad6725e2f4
Vikas Shrivastava <vikasrehli@gmail.com>
2023-01-24 14:20:51 +0000

    removed old src folder

--- src_old/main/resources/schema.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE cbs.cbsuser
-  (  
-     cbs_user_id	    int,
-     name               varchar(255),
-     password           varchar(255),
-     email              varchar(255),
-     PRIMARY KEY(cbs_user_id)
-  );
